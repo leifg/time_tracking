975d0ea99e70b40b7eef022f3f258bb0df5a832c
Leif Gensert <leif@leif.io>
2017-08-09 08:40:25 +0200

    fix: correctly provide timezone to timezone converter

--- lib/time_tracking/timezone_converter.ex
+++ lib/time_tracking/timezone_converter.ex
@@ -1,9 +1,11 @@
 defmodule TimezoneConverter do
-  @default_timezone Application.get_env(:time_tracking, :fastbill_timezone)
-
-  def convert(datetime_string, timezone \\ @default_timezone) do
+  def convert(datetime_string) do
     {:ok, datetime} = Timex.parse(datetime_string, "{ISO:Extended}")
-    {:ok, converted_string} = Timex.Timezone.convert(datetime, timezone) |> Timex.format("{ISO:Extended}")
+    {:ok, converted_string} = Timex.Timezone.convert(datetime, timezone()) |> Timex.format("{ISO:Extended}")
     converted_string
+  end
+
+  defp timezone do
+    Application.get_env(:time_tracking, :fastbill_timezone)
   end
 end
